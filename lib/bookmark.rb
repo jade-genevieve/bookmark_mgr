a97a7ab971a100fdd860f6ab18c6172c514b4332
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-11-06 15:10:49 +0000

    Testing database connection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,5 +1,5 @@
 require "pg"
-require "pg"
+require_relative "database_connection"
 
 class Bookmark
   attr_reader :id, :title, :url
@@ -11,15 +11,8 @@ class Bookmark
   end
 
   def self.all
-    if ENV["ENVIRONMENT"] == "test"
-      connection = PG.connect(dbname: "bookmarks_mgr_test")
-    else
-      connection = PG.connect(dbname: "bookmark_mgr")
-    end
-    result = connection.exec ("SELECT * FROM bookmarks;")
-    result.map { |bookmark|
-      Bookmark.new(id: bookmark["id"], title: bookmark["title"], url: bookmark["url"])
-    }
+    result = DatabaseConnection.query("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark["url"] }
   end
 
   def self.create(url:, title:)
@@ -53,11 +46,11 @@ class Bookmark
 
   def self.find(id:)
     if ENV["ENVIRONMENT"] == "test"
-        connection = PG.connect(dbname: "bookmarks_mgr_test")
-      else
-        connection = PG.connect(dbname: "bookmark_mgr")
-      end
-      result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
-      Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+      connection = PG.connect(dbname: "bookmarks_mgr_test")
+    else
+      connection = PG.connect(dbname: "bookmark_mgr")
+    end
+    result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+    Bookmark.new(id: result[0]["id"], title: result[0]["title"], url: result[0]["url"])
   end
 end
